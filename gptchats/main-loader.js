6b86c8b90332859e79c1a4f1a08e5be6dff2ae9a
FireyDeath4 <48757357+FireyDeath4@users.noreply.github.com>
2025-06-30 03:08:21 +0930

    Update main-loader.js

--- gptchats/main-loader.js
+++ gptchats/main-loader.js
@@ -2,8 +2,9 @@ var markdownToHTML,linkify,loadChat;
 import("/gptchats/importer.js").then((myModule)=>
   {markdownToHTML=myModule.default.markdownToHTML;
   linkify=myModule.default.linkify;
+  console.log(getPageName(location.href));
   loadChat("/gptchats/"+getPageName(location.href)
-    +"chat.json","#chatgpt-conversation")})
+    +"/chat.json","#chatgpt-conversation")})
 
 //https://stackoverflow.com/questions/30863164/extract-urls-directory-from-path-without-file-name-such-as-index-html
 function getPageName(url)
